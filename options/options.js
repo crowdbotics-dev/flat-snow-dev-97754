632200401ce7627c8fe0584cb83c179153f48264
Crowdbotics <engineering@crowdbotics.com>
2023-07-05 12:44:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-snow-dev-97754",
+  url: "https://flat-snow-dev-97754.botics.co",
+  api: "https://flat-snow-dev-97754.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
